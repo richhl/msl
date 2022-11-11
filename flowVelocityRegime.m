f15c9cc2116fb95b7159a911160de59d038a7625
Christian Hidalgo <rich@argonauta.org>
2022-11-11 13:39:40 +0100

    convenient matlab custom enumerations to classify flow regime and geometries

---
+++ flowVelocityRegime.m
@@ -0,0 +1,7 @@
+classdef flowVelocityRegime < uint16
+    enumeration 
+       subsonic (1)
+       transonic (2)
+       supersonic (3)
+    end
+end
---
+++ shapeType.m
@@ -0,0 +1,6 @@
+classdef shapeType  < uint16
+    enumeration 
+       flat (1)
+       cone (2)
+    end
+end
