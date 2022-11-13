1a1db4394dacdeb5a5383ddd4739b5810464a2b2
Christian Hidalgo <rich@argonauta.org>
2022-11-13 15:17:50 +0100

    done total Cd....TODO: review

--- multistage_launcher_drag.m
+++ multistage_launcher_drag.m
@@ -1,48 +1,37 @@
 %%Parasite Drag of the Launcher
 % Cd = CD Sumatory for each 'similar shape' geometry. 
 % being the geometries of the launcher:
-% 1. Warhead -Cofia : (Cd_shape + Cd_friction)
-% 2. Cylinder : (Cd_shape + Cd_friction)
-% 3. Cone trunk : (Cd_shape + Cd_friction)
-% 4. Tail Cylinder: (Cd_shape + Cd_friction + Cd_base)
+% 1. Warhead -Cofia : Cd_wave hipersonic || Cd_shape subsonic + Cd_friction
+% 2. Head Cylinder : Cd_friction
+% 3. Cone trunk : Cd_friction
+% 4. Tail Cylinder: Cd_friction + Cd_base
 
 %% Global data
 warhead_d = 3.5; %m
-reference_surface = pi*warhead_d^2/4; %m^2
+warhead_surface = pi*warhead_d^2/4; %m^2
+d_base = 5; %m
+reference_surface = pi*d_base^2/4; %m^2
 warhead_length = 4.6;
 cylinder1_length = 4.5; %m
 cylinder2_length = 10.7; %m
 cone_trunk_length = 5; %m cone trunk min diameter = d_warhead, max = d_base
 cylinder3_length = 6.6; %m
 cylinder4_length = 21; %m
-d_base = 5; %m
 total_length = warhead_length + cylinder1_length + cylinder2_length...
                 + cylinder3_length + cylinder4_length;
 slenderness = total_length/d_base; 
 
-
-
-clc
-
-%% Cd_warhead = Cd_shape_warhead + Cd_friction_warhead
-
-% Supersonic Cd_wave_warhead - warhead is replaced by a cone as
-% required by exercise text
-
+%% Mach range to analyse
 interval = 0.05;
 M_ = 0:interval:15; %mach vector points for plotting
 number_of_points = size(M_,2);
 transonic_start_point = cast((0.9+interval)/interval + 1,"int16"); %M>0.9<1.2 transonic
 supersonic_start_point = cast(1.2/interval + 1,"int16"); %M=1.2 supersonic
 
-%% Wave drag for launcher
-theta_cone = atan(warhead_d/(2*warhead_length))*180/pi;
-warhead_surface = pi*warhead_d/2*sqrt(warhead_length^2 + (warhead_d/2)^2);
-Cd_wave_vector = zeros(1,number_of_points);
+%%Go first for friction drag as we need it for subsonic shape drag calc 
 
 % raised https://moodle.upm.es/titulaciones/oficiales/mod/forum/discuss.php?d=9396#p15316
 % advised to use warhead, cylinder1+cylinder2, cone trunk, cylinder3+cylinder4
-
 % Friction Drag for warhead
 Cd_friction_wh_vector = zeros(1,number_of_points);
 reference_length = warhead_length;
@@ -63,7 +52,15 @@ for i=1:number_of_points
     Cd_friction_head_cylinder_vector(i) = Cd_friction_head_cylinder;
 end
 
-%% TODO: Cone trunk
+% Friction Drag for Cone trunk
+Cd_friction_cone_trunk_vector = zeros(1,number_of_points);
+reference_length = cone_trunk_length;
+wet_surface = pi * 1/2*(warhead_d + d_base) * sqrt(reference_length^2 + (1/2*(d_base - warhead_d)^2)); %surface in contact with the flow
+element_shape = shapeType.cone;
+for i=1:number_of_points
+    Cd_friction_cone_trunk = getFrictionDragCoefficient(M_(i), reference_length, element_shape, wet_surface, reference_surface);
+    Cd_friction_cone_trunk_vector(i) = Cd_friction_cone_trunk;
+end
 
 % Friction Drag for tail_cylinder = cylinder3 + cylinder4
 Cd_friction_tail_cylinder_vector = zeros(1,number_of_points);
@@ -72,62 +69,100 @@ wet_surface = pi * warhead_d * reference_length; %surface in contact with the fl
 element_shape = shapeType.flat;
 for i=1:number_of_points
     Cd_friction_tail_cylinder = getFrictionDragCoefficient(M_(i), reference_length, element_shape, wet_surface, reference_surface);
-    Cd_friction_tail_vector(i) = Cd_friction_tail_cylinder;
+    Cd_friction_tail_cylinder_vector(i) = Cd_friction_tail_cylinder;
 end
 
-%check for cylinder1+cylinder2
-% Cd_friction_cylinder_vector = zeros(1,number_of_points);
-% reference_length = cylinder1_length + cylinder2_length;
-% wet_surface = pi * warhead_d * reference_length; %surface in contact with the flow
-% element_shape = shapeType.flat;
-% for i=1:number_of_points
-%     Cd_friction_cylinder = getFrictionDragCoefficient(M_(i), reference_length, element_shape, wet_surface, reference_surface);
-%     Cd_friction_cylinder_vector(i) = Cd_friction_cylinder;
-% end
-% % test = Cd_friction_cylinder1_vector + Cd_friction_cylinder2_vector
+%Total friction drag is a weigthed sumatory of each 'similar shape geometry' launcher section 
+Cd_friction_vector = zeros(1,number_of_points);
+Cd_friction_vector = Cd_friction_wh_vector + Cd_friction_head_cylinder_vector + ....
+              Cd_friction_cone_trunk_vector + Cd_friction_tail_cylinder_vector;
 
-
-
-
-%% Cd_cylinder1 = Cd_shape_cylinder1 + Cd_friction_cylinder1
-% Friction Drag
-% l_cilynder1 = 4.5 + 10.7 + 6.6;
-% cyl1_surface = pi*warhead_d*l_cilynder1;
-% Cd_friction_cyl1_vector = zeros(1,number_of_points);
-% Turbulent flow applies as required by exercise text
-% for i=1:number_of_points
-%     Re_number = (rho0 * M_(i) * a0 * l_cilynder1) / nu;
-%     Cd_friction_incompres = 0.288 * log10( Re_number)^-2.45; %Turbulent incompressible
-%     if i<transonic_start_point
-%         Cd_friction_compres = Cd_friction_incompres * (1/(1+0.08*M_(i)*M_(i))); %Turbulent subsonic compressible
-%     elseif i>=supersonic_start_point
-%         Cd_friction_compres = Cd_friction_incompres * 1/(1+(gama-1)/2*M_(i)*M_(i))^0.467; %Turbulent supersonic compressible
-%     else continue
-%     end
-%     Cd_friction_mean_cyl1 = 1.25*Cd_friction_compres
-%     Cd_friction_cyl1 = Cd_friction_mean_cyl1 * cyl1_surface/ref_surface_geometry1;
-%     Cd_friction_cyl1_vector(i) = Cd_friction_cyl1
-% end
-
-Cd_friction = zeros(1,number_of_points);
-%Cd_friction = 
 
 % Subsonic Cd_shape = coefficient * Cd_friction_launcher? question
 % raised https://moodle.upm.es/titulaciones/oficiales/mod/forum/discuss.php?d=9396#p15316
-% advised to use first approach would use d_warhead + total_length to get coefficient value
+% advised to use  d_base + total_length to get coefficient value
 % for i=1:transonic_start_point-1
 %     Cd_shape =  (60/slenderness^3 + 0.0025*slenderness) * Cd_friction_vector(i);
 %     Cd_vector(i) = Cd_shape; 
 % end
+%% Wave drag for launcher
+theta_cone = atan(warhead_d/(2*warhead_length))*180/pi;
+warhead_surface = pi*warhead_d/2*sqrt(warhead_length^2 + (warhead_d/2)^2);
+Cd_wave_vector = zeros(1,number_of_points);
 for i=1:number_of_points
     geometry.theta_cone = theta_cone
-    Cd_wave_vector(i) = getWaveDrag(M_(i), shapeType.cone, geometry, slenderness, .003)
+    Cd_wave_vector(i) = getWaveDrag(M_(i), shapeType.cone, geometry, slenderness, Cd_friction_vector(i))
 end
 
-function drag_coefficient= getWaveDrag(mach_number, element_shape_type, geometry, slenderness, friction_drag_cf)
-% reference_length is mandatory for subsonic
-% friction_drag_cf is mandatory for subsonic points
+
+%% Base drag for launcher
+gas_outlet_surface = 0.8 * reference_surface;
+base_surface = reference_surface;
+Cd_base_vector = zeros(1,number_of_points);
+for i=1:number_of_points
+    Cd_base_vector(i) = getBaseDrag(M_(i), gas_outlet_surface, base_surface)
+end
+
+%% Total parasite drag
+Cd_total = Cd_wave_vector + Cd_friction_vector + Cd_base_vector;
+%Cd_total = Cd_wave_vector
+%Cd_total = Cd_friction_vector;
+%Cd_total = Cd_base_vector;
+
+%Transonic drag has to be a linear interpolation between sub-hyper mach
+%points drag values
+start_drag = Cd_total(transonic_start_point-1);
+end_drag = Cd_total(supersonic_start_point);
+number_of_transonic_points = cast(supersonic_start_point - transonic_start_point,'double');
+point_number = 1;
+for i=1:number_of_transonic_points
+    Cd_total(transonic_start_point + (i-1)) = start_drag + ((end_drag-start_drag)/(number_of_transonic_points+1)) * i;
+end
+
+%cd_figure = plot(M_(1:19), Cd_total(1:19),'b');
+%cd_figure = plot(M_(15:25), Cd_total(15:25),'b');
+%cd_figure = plot(M_(20:end), Cd_total(20:end),'b');
+cd_figure = plot(M_, Cd_total,'b');
+hold on;
+title("Cd parásita del lanzador en función de Nº de Mach");
+
+
+
+function drag_coefficient= getBaseDrag(mach_number, gas_outlet_surface, base_surface, engine_on)
+    arguments
+        mach_number
+        gas_outlet_surface
+        base_surface
+        engine_on = false
+    end
     flowKind = getFlowVelocityRegime(mach_number);
+    if flowVelocityRegime.transonic == flowKind
+        drag_coefficient=-1; % 'error value' for transonic
+        return 
+    end
+    
+    if flowKind == flowVelocityRegime.supersonic
+        switch engine_on
+            case false
+                drag_coefficient = 0.25/mach_number * (base_surface / base_surface);
+            case true
+                drag_coefficient = 0.25/mach_number * (base_surface - gas_outlet_surface / base_surface);
+        end
+    elseif flowKind == flowVelocityRegime.subsonic
+        switch engine_on
+            case false
+                drag_coefficient = (0.12 + 0.13*mach_number^2) * (base_surface / base_surface);
+            case true
+                drag_coefficient = (0.12 + 0.13*mach_number^2) * (base_surface - gas_outlet_surface / base_surface);
+        end
+    end
+end
+
+
+function drag_coefficient= getWaveDrag(mach_number, element_shape_type, geometry, slenderness, friction_drag_cf)
+% reference_length is mandatory for subsonic/transonic
+% friction_drag_cf is mandatory for subsonic/transonic points
+     flowKind = getFlowVelocityRegime(mach_number);
     if flowVelocityRegime.transonic == flowKind
         drag_coefficient=-1; % 'error value' for transonic
         return 
